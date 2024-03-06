7ccfa52296fb902f81f065c770a52064cff327e0
PetoAdam <peto.adam@yahoo.com>
2024-03-06 11:45:54 +0000

    changed physics to use pinocchio - added panda robot

--- PhysicsService/main.cpp
+++ PhysicsService/main.cpp
@@ -70,7 +70,7 @@ int main(int argc, char* argv[])
 		server.run(PORT_NUMBER);
 	});
 	
-	// Start an input thread that reads from the simulation
+	// Start an input thread that listens to the frontend (might want to use rest api instead for this?)
 	std::thread inputThread([&server, &mainEventLoop]()
 	{
 		
@@ -88,8 +88,8 @@ int main(int argc, char* argv[])
 	std::cout << "Main event loop started" << std::endl;
 
 	// Set up simulation
-	// TODO: change here to set up own URDF file or just pass it as an argument of this example.
-    const std::string urdf_filename = PINOCCHIO_MODEL_DIR + std::string("/ur10e/robot.urdf");
+	// TODO: add config file
+    const std::string urdf_filename = PINOCCHIO_MODEL_DIR + std::string("/panda/robot.urdf");
         
     // Load the urdf model
     Model model;
@@ -99,6 +99,10 @@ int main(int argc, char* argv[])
     // Create data required by the algorithms
     Data data(model);
 
+	// Define extra model variables
+	double damping_factor = -0.1;
+	double dt = 0.005;
+
     // Set initial robot data
     Eigen::VectorXd q = Eigen::VectorXd::Zero(model.nv);
     Eigen::VectorXd v = Eigen::VectorXd::Zero(model.nv);
@@ -106,43 +110,29 @@ int main(int argc, char* argv[])
     Eigen::VectorXd tau = Eigen::VectorXd::Zero(model.nv);
            
     auto startTime = std::chrono::high_resolution_clock::now();
-	Eigen::VectorXd tmp_a = Eigen::VectorXd::Zero(model.nv);
 
-	// Start simulation loop
+	// Main simulation loop
+	// TODO: add start/stop/reset/disconnect features and criteria (frotend too)
 	while (1)
 	{	
         // Generate joint positions
-        //a = pinocchio::aba(model, data, q, v, tau);
-
-		// Compute Coriolis and Gravity terms
-		Eigen::VectorXd c_g = pinocchio::nonLinearEffects(model, data, q, v);
-
-		// Compute the mass matrix M(q)
-        Eigen::MatrixXd M(model.nv, model.nv);
-        M.setZero();
-        for (size_t i = 0; i < model.nv; ++i)
-        {
-            // Set joint acceleration to 1 for joint i
-            tmp_a.setZero();
-            tmp_a[i] = 1.0;
-			//std::cout << pinocchio::rnea(model, data, q, Eigen::VectorXd::Zero(model.nv), tmp_a) << std::endl;
-			M.col(i) = (Eigen::VectorXd) pinocchio::rnea(model, data, q, Eigen::VectorXd::Zero(model.nv), tmp_a) - 
-						(Eigen::VectorXd) pinocchio::rnea(model, data, q, Eigen::VectorXd::Zero(model.nv), Eigen::VectorXd::Zero(model.nv));
-        }
-		//std::cout << "M:" << std::endl << M << std::endl;
-
-		// Solve for q_ddot
-		a = M.transpose().inverse() * (tau - c_g);
-        std::cout << "a: " << std::endl << a << std::endl;
-
-        // Measure iteration time
+		tau = damping_factor * v;
+        a = pinocchio::aba(model, data, q, v, tau);
+		
+		// Measure iteration time
         auto endTime = std::chrono::high_resolution_clock::now();
-        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
-        auto dt = elapsed.count() / 1000000.0; // Adjust dt dynamically based on iteration time
-        dt = 0.005;
-        v += a * dt;
-        q += v * dt;
-        //std::cout << "q: " << q.transpose() << "\tdt: " << dt <<std::endl;
+        auto elapsed = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime).count();
+		
+		v += a * dt;
+		q = pinocchio::integrate(model, q, v*dt);
+
+		// Calculate remaining time to sleep
+        auto remaining_time = static_cast<int>(dt * 1000000) - elapsed;
+        if (remaining_time > 0)
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(remaining_time));
+        }
+        std::clog<< "q: " << q.transpose() << "\tdt: " << dt <<std::endl;
 
         startTime = std::chrono::high_resolution_clock::now();
 
